9a1f3976afd275d163d7d5ac207024e9a8c94550
Shinta Sapta <saptarinishinta@gmail.com>
2016-11-25 07:35:44 +0700

    --all

---
+++ index.php
@@ -0,0 +1,407 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Kulina</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+  <!-- font proxima nova -->
+  <link rel="stylesheet" type="text/css" href="fonts/fonts.min.css" />
+
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+  <!-- main css -->
+  <link rel="stylesheet" type="text/css" href="stylesheets/main.css">
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+  </style>
+</head>
+<body>
+
+<!-- start navbar section -->
+<section class="navbar">
+  <nav class="navbar navbar-default navbar-fixed-top">  
+    
+    <!-- start navbar top -->
+      <div class="row navbar-top">
+          <div class="container">
+      
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                Sementara hanya melayani daerah Jakarta Pusat, Jakarta Selatan dan Jakarta Barat
+            </div>
+            <div class="col-md-6 col-sm-12 col-sm-12">
+              <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-custom pull-right">
+                  <li><a href="#">Dapur Mitra</a></li>
+                  <li><a href="#">Cara Pesan</a></li>
+                  <li><a href="#" class="text-red">Makanan Gratis</a></li>
+                  <li><a href="#">Masuk/Daftar</a></li>
+                </ul>
+              </div>
+            </div>
+          </div><!-- end container -->
+      </div>
+      <!--end navbar top -->
+
+      <!-- start navbar bottom -->
+      <div class="navbar-bottom">
+        <div class="container">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+            <div class="row">
+              <div class="col-md-3">
+                 <a class="navbar-brand" href="#"><h4>Logo Kulina</h4></a>
+              </div>
+              <div class="col-md-6 margin-less">
+                  <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                      <li class="activee"><a href="#">Langganan Kulina Box</a></li>
+                      <li><a href="#">Paket Katering</a></li>
+                      <li><a href="#">Menu Harian</a></li>
+                    </ul>
+                </div>
+              </div>
+            </div>
+        </div><!-- end container -->
+      </div> <!-- end navbar bottom -->
+     
+    </div>
+  </nav>
+</section>
+<!-- end navbar section -->
+
+<!-- start header section -->
+<section class="header">
+    <div class="container">
+        <div class="col-md-4 col-sm-12 col-xs-12 pull-right form-header">
+          <h3 class="text-center">Langganan Kulina Box</h3>
+          <h4 class="text-center text-explan">Paket makan siang (hari kerja Senin - Jumat)
+            murah,praktis, dan halal dimasak oleh dapur kateting profesional
+          </h4>
+          <hr class="red-line">
+          <div class="form-group">
+              <h5>Langganan berapa box per hari ?</h5>
+              <select class="form-control">
+                <option>5 Box atau lebih</option>
+                <option>10 Box atau lebih</option>
+              </select>
+              <h5>Langganan berapa hari ?</h5>
+              <select class="form-control">
+                <option>10 hari</option>
+                <option>20 hari</option>
+              </select>
+              <h3 class="text-center">Rp.19000/box</h3>
+              <p class="text-center">Harga Reguler Rp 30.000</p>
+              <h5 class="text-center text-red">Termasuk Biaya Kirim!</h5>
+              <button type="button" class="btn btn-default btn-header">Pesan Sekarang</button>
+        </div>
+    </div>
+  </div>
+</section>
+<!-- end header section -->
+
+<!-- start  intro section -->
+<section class="intro">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-sm-6 col-xs-12">
+          <div class="row">
+            <div class="col-md-2 col-sm-2 col-xs-12">
+              <img src="assets/icon/ic-deliver.png" class="">
+            </div>
+            <div class="col-md-10 col-sm-10 col-xs-12">
+              <h5>Pengiriman GRATIS</h5>
+              <p>Semua harga tercantum sudah termasuk biaya pengiriman, makana dikirim sebelum jam makan siang pada hari kerja ( Senin - Jumat)</p>
+            </div>
+            
+          </div>
+      </div>
+      <div class="col-md-6 col-sm-6 col-xs-12">
+          <div class="row">
+            <div class="col-md-2 col-sm-2 col-xs-12">
+              <img src="assets/icon/ic-guarantee.png" class="">
+            </div>
+            <div class="col-md-10 col-sm-10 col-xs-12">
+              <h5>Garansi Kualitas</h5>
+              <p>Berhenti langganan sewaktu-waktu dengan pengembalian penuh jika Anda tidak puas dengan makanan kami.</p>
+            </div>
+            
+          </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- end intro section -->
+    
+<!-- start about section -->
+<section class="package">
+  <div class="">
+    <div class="row">
+      <div class="col-md-6 col-sm-12 col-xs-12">
+        <div class="package-about">
+          <h3>Apa itu Kulina Box ? </h3>
+          <div class="col-sm-12 col-md-12 col-xs-12">
+             <hr class="red-line pull-left">
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, </p>
+
+          <h5>Apa saja yang ada di dalam Kulina BOx ? </h5>
+
+           <p>LSorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis  </p>
+           <ul>
+             <p><label><img src="assets/icon/ic-check.png"></label> 1 Porsi Nasi Putih</p>
+             <p><label><img src="assets/icon/ic-check.png"></label> 1 Porsi Lauk Utama</p>
+             <p><label><img src="assets/icon/ic-check.png"></label> 1 Porsi Lauk Pendamping</p>
+             <p><label><img src="assets/icon/ic-check.png"></label> 1 Porsi Sayur</p>
+           </ul>
+         </div>
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12 hidden-sm">
+          <img src="assets/bg/bg-about.png" class="img-responsive">
+      </div>
+    </div>
+  </div>  
+</section>
+<!-- end about section -->
+
+<!-- start menu section -->
+<section class="menu">
+  <div class=" bg-3">
+  <h3 class="text-center">Menu Untuk 2 Minggu</h3>
+  <hr class="red-line">
+
+  <div class="col-sm-10 col-sm-offset-1">
+    
+      <h3>AREA JAKARTA BARAT</h3>
+  </div>
+
+    <div class="row">
+      <div class="col-md-2 col-sm-2 col-xs-6 col-sm-offset-1">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-6">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-6">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-6">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-6">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+    </div>
+  </div><br>
+
+<div class="bg-3">
+
+  <div class="col-sm-10 col-sm-offset-1">
+    
+      <h3>AREA JAKARTA PUSAT,SELATAN, & TIMUR</h3>
+  </div>
+  
+  <div class="row">
+      <div class="col-md-2 col-sm-2 col-xs-6 col-sm-offset-1">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-6">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-6">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-6">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-6">
+        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        <p class="text-menu">Senin 18 Juli 2016</p>
+        <h5>Nasi Ayam, Perkedel, Cha Kangkung</h5>
+      </div>
+    </div>
+    
+    </div>
+  </div>
+</div><br><br>
+</section>
+<!-- start menu section -->
+
+<!-- start form-subsc section -->
+<section class="form-subsc">
+    <div class="container container-two text-center">
+        <div class="">
+          <p class="text-subsc">Daftarkan email Anda untuk mendapatkan voucher promo makan GRATIS Rp 50,000</p>
+          <form class="form-inline">
+              <div class="form-group">
+                <input class="form-control" id="email" placeholder="Nama">
+              </div>
+              <div class="form-group">
+                <input class="form-control" id="pwd" placeholder="Email">
+              </div>
+            <button type="submit" class="btn btn-default btn-subsc">Kirim</button>
+          </form>
+        </div>
+    </div>
+</section>
+<!-- end form section -->
+
+
+<!-- start  decsription section -->
+<section class="description">
+  <div class="container container-two text-center">
+    <h3>Mengapa Langganan Kulina Box ?</h3>
+      <div class="row">
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <img src="assets/icon/ic-kualitas.png" ><br>
+            <h5>Kualitas & Variasi Terjamin</h5>
+            <p>Masakan anda dimasak oleh apur katering profesional dengan variasi menu berbeda setiap harinya dalam pengawasan kualitas Kulina</p>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <img src="assets/icon/ic-harga.png" ><br>
+            <h5>Harga Terjangkau</h5>
+            <p>Harga lebih murah dibandingkan makanan yang biasa anda beli di tempat makan dengan kualitaslebih baik. Pesan banyak lebih murah.</p>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <img src="assets/icon/ic-fleksibel.png" ><br>
+            <h5>Fleksibel</h5>
+            <p>Anda bisa berhenti berangganan atau melewati jadwal sementara atau melewati sewaktu-waktu tanpa kehilangan masa berlanggan.</p>
+       </div><!-- end row -->
+        </div>
+      </div>
+  </div>
+</section>
+<!-- end  section -->
+
+<!-- start sample section -->
+<section class="sample">
+  <div class="">
+    <div class="row">
+       <div class="col-md-6 col-sm-12 col-xs-12 hidden-sm">
+          <img src="assets/bg/bg-desc.png" class="img-responsive">
+       </div>
+        <div class="col-md-6 col-sm-12 col-xs-12 sample-box text-center container">
+          <h3>Penasaran dengan Kulina Box ? </h3>
+          <h3>COBA GRATIS</h3>
+           <button type="button" class="btn btn-primary">SAYA INGIN SAMPLE GRATIS</button>
+        </div>
+    </div>
+  </div>  
+</section><!-- end row -->
+<!-- end sample section -->
+
+<!-- start  people section -->
+<section class="people">
+  <div class="container text-center">
+      <div class="row">
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <img src="assets/icon/icon-people/ic-women.png" ><br>
+            <h4 class="text-explan">"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes" </h4>
+            <h5> -- Dyah Ayu Yaya, Jakarta Selatan</h5>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <img src="assets/icon/icon-people/ic-man-one.png" ><br>
+             <h4 class="text-explan">"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes" </h4>
+             <h5> -- Dyah Ayu Yaya, Jakarta Selatan</h5>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <img src="assets/icon/icon-people/ic-man-two.png" ><br>
+             <h4 class="text-explan">"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes" </h4>
+             <h5> -- Dyah Ayu Yaya, Jakarta Selatan</h5>
+       </div><!-- end row -->
+        </div>
+      </div>
+  </div>
+</section>
+<!-- end  people section -->
+
+
+<!-- start  footer section -->
+<section class="footer">
+  <div class="container">
+    <!-- start row -->
+    <div class="row">
+      <div class="col-md-3">
+        <h5>KULINA OFFICE</h5>
+        <h6>Bits Coworking Space,Midtown lt 3</h6>
+        <h6>Jl. Tulodong Atas No.28, Senayan,</h6>
+        <h6>Kpy. Baru, Jakarta Selatan</h6>
+        <h6>DKI Jakarta</h6>
+        
+      </div>
+      <div class="col-md-2">
+        <h5>TENTANG KULINA</h5>
+        <h6>Kebijakan Privasi</h6>
+        <h6>Syarat & Ketentuan</h6>
+        <h6>FAQ</h6>
+        <h6>Blog</h6>
+        
+      </div>
+      <div class="col-md-2">
+        <h5>BERGABUNG</h5>
+         <h6>Daftar jadi Dapur Mitra</h6>
+         <h6>Referensikan Chef Favoritmu</h6>
+         <h6>Karir di Kulina</h6>
+
+      </div>
+      <div class="col-md-5">
+        <h5>METODE PEMBAYARAN</h5>
+      </div>
+    </div>
+    <!-- end row -->
+    <div class="container">
+    <!-- start row -->
+  </div><!-- end container -->
+</section>
+<!-- end  footer section -->
+
+<section class="copyright">
+  <div class="container">
+    <div class="row copyright">
+      <div class="col-md-10">
+        <h6>Made with from Indonesia &copy 2016 Kulina -- All rights reserved </h6>
+      </div>
+      <div class="col-md-2">
+       
+      </div>
+    </div><!-- end row -->
+  </div>
+</section>
+
+</body>
+</html>
+
